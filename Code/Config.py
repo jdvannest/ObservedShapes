8394f65d13fe335b743b5f07379c93fccaf2b407
jdvannest (macbook) <jordanvannest@gmail.com>
2023-01-24 15:31:06 -0600

    Initial commit

---
+++ Code/Config.py
@@ -0,0 +1,90 @@
+import os,pickle
+
+loop,rewrite = True,False
+while loop:
+    query = input('Remake Image Directory? (y/n): ')
+    if query in ['y','Y']:
+        rewrite=True
+        loop=False
+    elif query in ['n','N']:
+        loop=False
+    else: print('Invalid input.')
+
+
+config = {
+    #Path to directory where datafiles are written
+    'output_path' : 'Data/',
+    #Path to prefered python executable
+    'python_path' : '/myhome2/users/vannest/anaconda3/bin/python'
+}
+
+out = open('Config.pickle','wb') 
+pickle.dump(config,out)
+out.close()
+
+
+marvel_path = '/myhome2/users/munshi/dwarf_volumes/'
+dcjl_path = '/myhome2/users/munshi/e12gals/'
+
+Sims = {
+    'cptmarvel' : {
+        'path' : marvel_path+'cptmarvel.cosmo25cmb.4096g5HbwK1BH/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096',
+        'halos' : [1,2,3,5,6,7,10,11,13,14,24]
+    },
+    'elektra' : {
+        'path' : marvel_path+'elektra.cosmo25cmb.4096g5HbwK1BH/elektra.cosmo25cmb.4096g5HbwK1BH.004096/elektra.cosmo25cmb.4096g5HbwK1BH.004096',
+        'halos' : [1,2,3,4,5,8,9,10,11,12,17,36,64]
+    },
+    'storm' : {
+        'path' : marvel_path+'storm.cosmo25cmb.4096g5HbwK1BH/storm.cosmo25cmb.4096g5HbwK1BH.004096/storm.cosmo25cmb.4096g5HbwK1BH.004096',
+        'halos' : [1,2,3,4,5,6,7,8,10,11,12,14,15,22,23,31,37,44,48,55,118]
+    },
+    'rogue' : {
+        'path' : marvel_path+'rogue.cosmo25cmb.4096g5HbwK1BH/rogue.cosmo25cmb.4096g5HbwK1BH.004096/rogue.cosmo25cmb.4096g5HbwK1BH.004096',
+        'halos' : [1,3,7,8,10,11,12,15,16,17,28,31,37,58,116]
+    }
+    # 'h148' : {
+    #     'path' : dcjl_path+'h148.cosmo50PLK.3072g3HbwK1BH/h148.cosmo50PLK.3072g3HbwK1BH.004096/h148.cosmo50PLK.3072g3HbwK1BH.004096',
+    #     'halos' : [2,3,4,6,7,11,12,13,15,20,23,27,28,29,33,34,37,38,41,43,51,59,65,75,86,94,109,114,122]
+    # },
+    # 'h229' : {
+    #     'path' : dcjl_path+'h229.cosmo50PLK.3072gst5HbwK1BH/h229.cosmo50PLK.3072gst5HbwK1BH.004096/h229.cosmo50PLK.3072gst5HbwK1BH.004096',
+    #     'halos' : [2,3,6,14,15,18,20,22,25,33,47,48,49,52,57,62,89,92,127]
+    # },
+    # 'h242' : {
+    #     'path' : dcjl_path+'h242.cosmo50PLK.3072gst5HbwK1BH/h242.cosmo50PLK.3072gst5HbwK1BH.004096/h242.cosmo50PLK.3072gst5HbwK1BH.004096',
+    #     'halos' : [8,10,21,26,30,34,38,42,44,45,63,70,81,138]
+    # },
+    # 'h329' : {
+    #     'path' : dcjl_path+'h329.cosmo50PLK.3072gst5HbwK1BH/h329.cosmo50PLK.3072gst5HbwK1BH.004096/h329.cosmo50PLK.3072gst5HbwK1BH.004096',
+    #     'halos' : [7,29,30,37,53,92,115,117,127]
+    # }
+}
+
+out = open('SimulationInfo.BW.pickle','wb') 
+pickle.dump(Sims,out)
+out.close()
+
+#Create Image Directory
+if rewrite:
+    os.system('rm -rf ../Images/*')
+    for s in Sims:
+        os.system(f'mkdir ../Images/{s}.BW')
+        for h in Sims[s]['halos']: os.system(f'mkdir ../Images/{s}.BW/{h}')
+
+Sims = {
+    'storm' : {
+        'path' : marvel_path+'storm.cosmo25cmb.4096g1HsbBH/storm.cosmo25cmb.4096g1HsbBH.004096/storm.cosmo25cmb.4096g1HsbBH.004096',
+        'halos' : [1,2,3,4,5,6,7,8,10,11,12,14,15,19,21,23,25,41,42,49,61,82,262,272,273,472,541]
+    }
+}
+
+out = open('SimulationInfo.SB.pickle','wb') 
+pickle.dump(Sims,out)
+out.close()
+
+#Create Image Directory
+if rewrite:
+    for s in Sims:
+        os.system(f'mkdir ../Images/{s}.SB')
+        for h in Sims[s]['halos']: os.system(f'mkdir ../Images/{s}.SB/{h}')
---
+++ Code/ImageCollection.py
@@ -0,0 +1,73 @@
+import argparse,pickle,os,pynbody,sys,time
+import numpy as np
+import matplotlib.pylab as plt
+from pynbody.plot.sph import image
+from multiprocessing import Manager,Pool
+def myprint(string,clear=False):
+    if clear:
+        sys.stdout.write("\033[F")
+        sys.stdout.write("\033[K") 
+    print(string)
+
+parser = argparse.ArgumentParser(description='Collect images of all resolved halos from a given simulation. Images will be generated across all orientations.')
+parser.add_argument('-f','--feedback',choices=['BW','SB'],default='BW',help='Feedback Model')
+parser.add_argument('-s','--simulation',choices=['cptmarvel','elektra','storm','rogue'],required=True,help='Simulation to analyze')
+parser.add_argument('-n','--numproc',type=int,required=True,help='Number of processors to use')
+parser.add_argument('-o','--overwrite',action='store_true',help='Overwrite existing images')
+args = parser.parse_args()
+
+SimInfo = pickle.load(open(f'SimulationInfo.{args.feedback}.pickle','rb'))
+simpath = SimInfo[args.simulation]['path']
+halos = SimInfo[args.simulation]['path']
+dx,dy = 30,30 #Angular resolution of rotations
+#Check if all halos in sim have been completed
+if f'{halos[-1]}.x{180-dx:03d}.y{360-dy}.png' in os.listdir(f'../Images/{args.simulation}.{args.feedback}/{halos[-1]}/') and not args.overwrite:
+    print(f'{args.simulation} completed.')
+    sys.exit(0)
+
+print(f'Loading {args.simulation}')
+tstart = time.time()
+sim = pynbody.load(simpath)
+sim.physical_units()
+h = sim.halos()
+myprint(f'{args.simulation} loaded.',clear=True)
+
+def GenerateImages(hid,progress):
+    #Check if halo has been complete
+    if f'{hid}.x{180-dx:03d}.y{360-dy}.png' in os.listdir(f'../Images/{args.simulation}.{args.feedback}/{hid}/') and not args.overwrite:
+        progress[0]+=1
+        myprint(f'\tGenerating images: {round(prog/len(halos)*100,2)}%',clear=True)
+        return
+    halo = h[hid]
+    pynbody.analys.angmom.faceon(halo)
+    Rhalf = pynbody.analysis.luminosity.half_light_r(halo)
+    width = 3*Rhalf
+    ImageSpace = pynbody.filt.Sphere(width*np.sqrt(2)*1.01)
+    xrotation,yrotation = 0,0
+
+    while xrotation*dx<180:
+        while yrotation*dy<360:
+            if f'{hid}.x{xrotation*dx:03d}.y{yrotation*dy:03d}.png' not in os.listdir(f'../Images/{args.simulation}.{args.feedback}/{hid}/') or args.overwrite:
+                f = plt.figure(frameon=False)
+                f.set_size_inches(10,10)
+                ax = plt.Axes(f, [0., 0., 1., 1.])
+                ax.set_axis_off()
+                f.add_axes(ax)
+                im = image(sim[ImageSpace].s,qty='v_lum_den',width=width,subplot=ax,units='kpc^-2',resolution=1000,show_cbar=False)
+                f.savefig(f'../Images/{args.simulation}.{args.feedback}/{hid}/{hid}.x{xrotation*dx:03d}.y{yrotation*dy:03d}.png')
+                plt.close()
+            halo.rotate_y(dy)
+            yrotation+=1
+        halo.rotate_x(dx)
+        xrotation+=1
+    progress[0]+=1
+    myprint(f'\tGenerating images: {round(prog/len(halos)*100,2)}%',clear=True)
+
+with Manager() as manager:
+    prog = manager.list([0])
+    print(f'\tGenerating images: {round(prog/len(halos)*100,2)}%')
+    p = Pool(args.numproc)
+    p.map(GenerateImages,halos)
+
+tstop = time.time()
+print(f'{args.simulation} completed in {round((tstop-tstart)/60,2)} minutes.')
