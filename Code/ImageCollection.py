12c8ee4d9ca3a427874b44691cd7afe1ef8c4a5f
jdvannest (macbook) <jordanvannest@gmail.com>
2023-01-24 15:50:23 -0600

    Updated ImageCollection.py. Added tracking file to Images directory.

--- Code/ImageCollection.py
+++ Code/ImageCollection.py
@@ -32,40 +32,39 @@ sim.physical_units()
 h = sim.halos()
 myprint(f'{args.simulation} loaded.',clear=True)
 
-def GenerateImages(hid,progress):
-    #Check if halo has been complete
-    if f'{hid}.x{180-dx:03d}.y{360-dy}.png' in os.listdir(f'../Images/{args.simulation}.{args.feedback}/{hid}/') and not args.overwrite:
-        progress[0]+=1
-        myprint(f'\tGenerating images: {round(prog/len(halos)*100,2)}%',clear=True)
-        return
-    halo = h[hid]
-    pynbody.analys.angmom.faceon(halo)
-    Rhalf = pynbody.analysis.luminosity.half_light_r(halo)
-    width = 3*Rhalf
-    ImageSpace = pynbody.filt.Sphere(width*np.sqrt(2)*1.01)
-    xrotation,yrotation = 0,0
-
-    while xrotation*dx<180:
-        while yrotation*dy<360:
-            if f'{hid}.x{xrotation*dx:03d}.y{yrotation*dy:03d}.png' not in os.listdir(f'../Images/{args.simulation}.{args.feedback}/{hid}/') or args.overwrite:
-                f = plt.figure(frameon=False)
-                f.set_size_inches(10,10)
-                ax = plt.Axes(f, [0., 0., 1., 1.])
-                ax.set_axis_off()
-                f.add_axes(ax)
-                im = image(sim[ImageSpace].s,qty='v_lum_den',width=width,subplot=ax,units='kpc^-2',resolution=1000,show_cbar=False)
-                f.savefig(f'../Images/{args.simulation}.{args.feedback}/{hid}/{hid}.x{xrotation*dx:03d}.y{yrotation*dy:03d}.png')
-                plt.close()
-            halo.rotate_y(dy)
-            yrotation+=1
-        halo.rotate_x(dx)
-        xrotation+=1
-    progress[0]+=1
-    myprint(f'\tGenerating images: {round(prog/len(halos)*100,2)}%',clear=True)
-
 with Manager() as manager:
     prog = manager.list([0])
-    print(f'\tGenerating images: {round(prog/len(halos)*100,2)}%')
+    print(f'\tGenerating images: {round(prog[0]/len(halos)*100,2)}%')
+    def GenerateImages(hid):
+        #Check if halo has been complete
+        if f'{hid}.x{180-dx:03d}.y{360-dy}.png' in os.listdir(f'../Images/{args.simulation}.{args.feedback}/{hid}/') and not args.overwrite:
+            prog[0]+=1
+            myprint(f'\tGenerating images: {round(prog/len(halos)*100,2)}%',clear=True)
+            return
+        halo = h[hid]
+        pynbody.analys.angmom.faceon(halo)
+        Rhalf = pynbody.analysis.luminosity.half_light_r(halo)
+        width = 3*Rhalf
+        ImageSpace = pynbody.filt.Sphere(width*np.sqrt(2)*1.01)
+        xrotation,yrotation = 0,0
+
+        while xrotation*dx<180:
+            while yrotation*dy<360:
+                if f'{hid}.x{xrotation*dx:03d}.y{yrotation*dy:03d}.png' not in os.listdir(f'../Images/{args.simulation}.{args.feedback}/{hid}/') or args.overwrite:
+                    f = plt.figure(frameon=False)
+                    f.set_size_inches(10,10)
+                    ax = plt.Axes(f, [0., 0., 1., 1.])
+                    ax.set_axis_off()
+                    f.add_axes(ax)
+                    im = image(sim[ImageSpace].s,qty='v_lum_den',width=width,subplot=ax,units='kpc^-2',resolution=1000,show_cbar=False)
+                    f.savefig(f'../Images/{args.simulation}.{args.feedback}/{hid}/{hid}.x{xrotation*dx:03d}.y{yrotation*dy:03d}.png')
+                    plt.close()
+                halo.rotate_y(dy)
+                yrotation+=1
+            halo.rotate_x(dx)
+            xrotation+=1
+        prog[0]+=1
+        myprint(f'\tGenerating images: {round(prog[0]/len(halos)*100,2)}%',clear=True)
     p = Pool(args.numproc)
     p.map(GenerateImages,halos)
 
