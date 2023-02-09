c1acaa32163f91190c41ed6e23aed2d3340d1c76
jdvannest (macbook) <jordanvannest@gmail.com>
2023-02-09 16:37:10 -0600

    Finished IsophoteMasking code. Masking can begin after we derive RMS threshold

--- Code/IsophoteMasking.py
+++ Code/IsophoteMasking.py
@@ -1,4 +1,4 @@
-import pickle,sys,warnings
+import argparse,pickle,sys,warnings
 import numpy as np
 import matplotlib.pylab as plt
 from math import pi,degrees
@@ -10,6 +10,17 @@ from skimage.measure import EllipseModel
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 plt.rcParams.update({'text.usetex':False})
 warnings.filterwarnings("ignore")
+def myprint(string,clear=False):
+    if clear:
+        sys.stdout.write("\033[F")
+        sys.stdout.write("\033[K") 
+    print(string)
+
+parser = argparse.ArgumentParser(description='Collect images of all resolved halos from a given simulation. Images will be generated across all orientations.')
+parser.add_argument('-f','--feedback',choices=['BW','SB'],default='BW',help='Feedback Model')
+parser.add_argument('-s','--simulation',choices=['cptmarvel','elektra','storm','rogue'],required=True,help='Simulation to analyze')
+#parser.add_argument('-o','--overwrite',action='store_true',help='Overwrite existing images')
+args = parser.parse_args()
 
 #Masking Functions
 def MaskCircle(rad,cen_x,cen_y,isophote,mode):
@@ -48,7 +59,7 @@ def MaskSlice(minang,maxang,isophote,mode):
 def InitializeGUI(PlotName):
     #GUI Properties
     _VARS = {'window':False,'fig_agg':False,'pltFig':False}#,'a':np.nan,'b':np.nan}
-    plt.style.use('Solarize_Light2')
+    #plt.style.use('Solarize_Light2')
     GuiFont = 'Any 16'
     GuiColor = '#E8E8E8'
     sg.theme('black')
@@ -163,20 +174,22 @@ def draw_figure(canvas, figure):
         return figure_canvas_agg
 def drawChart():
     _VARS['pltFig'] = plt.figure()
+    plt.imshow(LogImage)
     plt.grid(b=None)
     _VARS['pltFig'].axes[0].set_xlim([0,1000])
     _VARS['pltFig'].axes[0].set_ylim([0,1000])
-    plt.imshow(im)
+    plt.grid(b=None)
     plt.scatter(500,500,marker='+',s=10**2,c='w')
     plt.scatter(iso[1],iso[0],c='r',s=.5**2)
     _VARS['fig_agg'] = draw_figure(_VARS['window']['figCanvas'].TKCanvas,_VARS['pltFig'])
 def updateChart(DrawCircle=False,DrawAngle=False,DrawEllipse=False):
     _VARS['fig_agg'].get_tk_widget().forget()
     plt.clf()
+    plt.imshow(LogImage)
     plt.grid(b=None)
     _VARS['pltFig'].axes[0].set_xlim([0,1000])
     _VARS['pltFig'].axes[0].set_ylim([0,1000])
-    plt.imshow(im)
+    plt.grid(b=None)
     plt.scatter(500,500,marker='+',s=10**2,c='w')
     plt.scatter(iso[1],iso[0],c='r',s=.5**2)
     if DrawCircle:
@@ -204,60 +217,114 @@ def updateChart(DrawCircle=False,DrawAngle=False,DrawEllipse=False):
         plt.plot([-a*cos(phi)+cen[0],a*cos(phi)+cen[0]],[-a*sin(phi)+cen[1],a*sin(phi)+cen[1]],color='orange')
         plt.plot([-b*cos(phi+pi/2)+cen[0],b*cos(phi+pi/2)+cen[0]],[-b*sin(phi+pi/2)+cen[1],
                    b*sin(phi+pi/2)+cen[1]],color='orange')   
-        Data[halo][view] =  min([a,b])/max([a,b])
     _VARS['fig_agg'] = draw_figure(_VARS['window']['figCanvas'].TKCanvas,_VARS['pltFig'])
+#Create new Isophote Plot
+def RMS(res):
+    if not isinstance(res,np.ndarray): res = np.array(res)
+    return( np.sqrt(sum(res**2)/len(res)) )
+def SavePlot(logimage,iso,fname):
+    f,ax = plt.subplots(1,1)
+    ax.imshow(logimage)
+    ax.set_xlim([0,1e3])
+    ax.set_ylim([1e3,0])
+    ax.scatter(500,500,c='k',marker='+')
+    if len(iso[0])>0:
+        ax.scatter(iso[1],iso[0],c='r',marker='.',s=1)
+        xy=np.zeros((len(iso[0]),2))
+        for idx in range(len(iso[0])):
+            xy[idx]=[iso[1][idx],iso[0][idx]]
+        #Fit ellipse
+        E = EllipseModel()
+        E.estimate(np.array(xy))
+        params = E.params
+        cen = np.array([params[0],params[1]])
+        phi = params[4]
+        a,b = params[2],params[3]
+        #a = max([params[2],params[3]])
+        #b = min([params[2],params[3]])
+        residual = E.residuals(np.array(xy))
+        rms = RMS(residual)
+        #Plot Ellipse and Fit Results
+        ax.add_patch(Ellipse(cen,2*a,2*b,angle=degrees(phi),facecolor='None',edgecolor='orange'))
+        plt.plot([-a*cos(phi)+cen[0],a*cos(phi)+cen[0]],[-a*sin(phi)+cen[1],a*sin(phi)+cen[1]],color='orange')
+        plt.plot([-b*cos(phi+pi/2)+cen[0],b*cos(phi+pi/2)+cen[0]],[-b*sin(phi+pi/2)+cen[1],b*sin(phi+pi/2)+cen[1]],color='orange')
+        atrue,btrue = max([a,b]),min([a,b])
+        ax.set_title(f'b/a: {round(btrue/atrue,3)}  RMS: {round(rms,3)}  Manual: True',fontsize=15)
+        f.savefig(fname,bbox_inches='tight',pad_inches=.1)
+        return(btrue/atrue)
+    else:
+        ax.set_title(f'b/a: NaN  RMS: NaN  Manual: True',fontsize=15)
+        f.savefig(fname,bbox_inches='tight',pad_inches=.1)
+        return(np.NaN)
 
 
-Images = pickle.load(open('../DataFiles/ObservedShapes.ImageData.pickle','rb'))
-outfile = '../DataFiles/ObservedShapes.ShapeData.pickle'
-Data = {}
+#Load sim-level data
+Images = pickle.load(open(f'../Data/{args.simulation}.{args.feedback}.Images.pickle','rb'))
+ShapeData = pickle.load(open(f'../Data/{args.simulation}.{args.feedback}.ShapeData.pickle','rb'))
+Masking = pickle.load(open(f'../Data/{args.simulation}.{args.feedback}.Masking.pickle','rb'))
+Profiles = pickle.load(open(f'../Data/{args.simulation}.{args.feedback}.Profiles.pickle','rb'))
+test = pickle.load(open(f'../Data/{args.simulation}.{args.feedback}.ShapeData.pickle','rb'))
 
-for halo in Images:
-    Data[halo] = {}
-    for view in ['Faceon','Sideon']:
-        Data[halo][view] = np.nan
-        v,im = Images[halo][f'SB_{view}'],Images[halo][f'Im_{view}']
-        tol = .01
-        iso = np.where((im>v*(1-tol)) & (im<v*(1+tol)))
+for halo in Masking:
+    for rotation in Masking[halo]:
+        if Masking[halo][rotation]:
+            print(f'Masking {args.simulation} {halo}-{rotation}...')
+            #Load halo-level data
+            Image = Images[halo][rotation]
+            Image = np.flip(Image,0)
+            LogImage = plt.imread(f'../Images/{args.simulation}.{args.feedback}/{halo}/{halo}.{".y".join(rotation.split("y"))}.png')
 
-        DC,DA,DE = False,False,False
-        _VARS = InitializeGUI(f'Halo {halo} - {view}')
-        drawChart()
-        while True:
-            event, values = _VARS['window'].read()
-            if event in [sg.WIN_CLOSED,'Exit']:
-                print('Aborting Code')
-                sys.exit(0)
-            if event in ['Ignore','Save']:
-                break
-            if event=='Reset':
-                tol = float(values['Iso%'])/100
-                iso = np.where((im>v*(1-tol)) & (im<v*(1+tol)))
-                DC,DA,DE = False,False,False
-                updateChart(DrawCircle=DC,DrawAngle=DA,DrawEllipse=DE)
-            if event=='Mask':
-                DE = False
-                if DC:
-                    iso = MaskCircle(values['RadiusAdjust'],cen_x=values['CenXAdjust'],
-                            cen_y=values['CenYAdjust'],isophote=iso,mode=values['CMode'][0])
-                if DA:
-                    iso = MaskSlice(values['MinAngle'],values['MaxAngle'],
-                            isophote=iso,mode=values['AMode'][0])
-                updateChart(DrawCircle=DC,DrawAngle=DA,DrawEllipse=DE)
-            if event in ['RadiusAdjust','CenXAdjust','CenYAdjust'] and values['CMode']!=[]:
-                DC,DE = True,False
-                updateChart(DrawCircle=DC,DrawAngle=DA,DrawEllipse=DE)
-            if event in ['MinAngle','MaxAngle'] and values['AMode']!=[]:
-                DA,DE = True,False
-                updateChart(DrawCircle=DC,DrawAngle=DA,DrawEllipse=DE)
-            if event=='Fit Ellipse':
-                DE = True
-                updateChart(DrawCircle=DC,DrawAngle=DA,DrawEllipse=True)
-        plt.close()
-        _VARS['window'].close()
+            #Find default isophote
+            rbins = Profiles[halo][rotation]['rbins']
+            Rhalf = Profiles[halo][rotation]['Rhalf']
+            ind_eff = np.argmin(abs(rbins-Rhalf))
+            v = Profiles[halo][rotation]['v_lum_den'][ind_eff]
+            tol = .01
+            iso = np.where((Image>v*(1-tol)) & (Image<v*(1+tol)))
 
-#out = open(outfile,'wb')
-#pickle.dump(Data,out)
-#out.close()
-#print('File Saved.')
-print(Data)
+            DC,DA,DE = False,False,False
+            _VARS = InitializeGUI(f'Halo {halo} - {rotation}')
+            drawChart()
+            while True:
+                event, values = _VARS['window'].read()
+                if event in [sg.WIN_CLOSED,'Exit']:
+                    print('Aborting Code')
+                    sys.exit(0)
+                if event in ['Save','Ignore']:
+                    _VARS['window'].close()
+                    myprint(f'Saving {args.simulation} {halo}-{rotation}...',clear=True)
+                    if event=='Ignore':iso=[[],[]]
+                    ba = SavePlot(LogImage,iso,f'../Images/{args.simulation}.{args.feedback}/{halo}/{halo}.{".y".join(rotation.split("y"))}.Isophote.png')
+                    ShapeData[halo][rotation] = ba
+                    pickle.dump(ShapeData,open(f'../Data/{args.simulation}.{args.feedback}.ShapeData.pickle','wb'))
+                    Masking[halo][rotation] = False
+                    pickle.dump(Masking,open(f'../Data/{args.simulation}.{args.feedback}.Masking.pickle','wb'))
+                    myprint(f'{args.simulation} {halo}-{rotation} saved.',clear=True)
+                    break
+                if event=='Reset':
+                    tol = float(values['Iso%'])/100
+                    iso = np.where((Image>v*(1-tol)) & (Image<v*(1+tol)))
+                    DC,DA,DE = False,False,False
+                    updateChart(DrawCircle=DC,DrawAngle=DA,DrawEllipse=DE)
+                if event=='Mask':
+                    DE = False
+                    if DC:
+                        iso = MaskCircle(values['RadiusAdjust'],cen_x=values['CenXAdjust'],
+                                cen_y=values['CenYAdjust'],isophote=iso,mode=values['CMode'][0])
+                    if DA:
+                        iso = MaskSlice(values['MinAngle'],values['MaxAngle'],
+                                isophote=iso,mode=values['AMode'][0])
+                    updateChart(DrawCircle=DC,DrawAngle=DA,DrawEllipse=DE)
+                if event in ['RadiusAdjust','CenXAdjust','CenYAdjust'] and values['CMode']!=[]:
+                    DC,DE = True,False
+                    updateChart(DrawCircle=DC,DrawAngle=DA,DrawEllipse=DE)
+                if event in ['MinAngle','MaxAngle'] and values['AMode']!=[]:
+                    DA,DE = True,False
+                    updateChart(DrawCircle=DC,DrawAngle=DA,DrawEllipse=DE)
+                if event=='Fit Ellipse':
+                    DE = True
+                    updateChart(DrawCircle=DC,DrawAngle=DA,DrawEllipse=True)
+            plt.close()
+            _VARS['window'].close()
+
+print(f'No halos in {args.simulation} need masking.')
