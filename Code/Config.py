02e64aa8b0e47b98dea660986c440a0f0d2d7a38
jdvannest (macbook) <jordanvannest@gmail.com>
2023-07-01 01:26:40 -0400

    Masked storm BW

--- Code/Config.py
+++ Code/Config.py
@@ -37,7 +37,7 @@ Sims = {
     },
     'storm' : {
         'path' : marvel_path+'storm.cosmo25cmb.4096g5HbwK1BH/storm.cosmo25cmb.4096g5HbwK1BH.004096/storm.cosmo25cmb.4096g5HbwK1BH.004096',
-        'halos' : [1,2,3,4,5,6,7,8,12,14,15,22,23,31,37,44,48,55,118]#,10,11]
+        'halos' : [1,2,3,4,5,6,7,8,14,15,22,23,31,37,44,48,55,118]#,10,11,12]
     },
     'rogue' : {
         'path' : marvel_path+'rogue.cosmo25cmb.4096g5HbwK1BH/rogue.cosmo25cmb.4096g5HbwK1BH.004096/rogue.cosmo25cmb.4096g5HbwK1BH.004096',
